afb8f19ca5e9497c98ebec55ed2a06d908b81680
Yueqiu Wang <ge23jiv@tum.de>
2023-06-20 11:56:12 +0200

    Upload evaluation function

---
+++ evaluate.m
@@ -0,0 +1,11 @@
+function accuracy = evaluate(YPred,YTest)
+
+
+% compute the accuracy of the model
+iscorrect = YPred == YTest;
+accuracy = sum(iscorrect)/numel(iscorrect);
+
+% plot confusion chart
+confusionchart(YTest,YPred);
+
+end
