ae56be4be3279bb0ddd60fff125d255af4392a26
Yueqiu Wang <ge23jiv@tum.de>
2023-07-10 21:05:32 +0200

    add comment

--- KNN/classifyWalk_knn.m
+++ KNN/classifyWalk_knn.m
@@ -1,4 +1,12 @@
 function YPred = classifyWalk_knn(model, XTest)
+% This function is used to classify the test data using the trained knn model.
+%
+% INPUTS:
+% - model: trained k-NN model
+% - XTest: test data
+%
+% OUTPUT:
+% - YPred: the predicted label from test data.
 
 % Step 1: extract features of the test data
 features = extractFeatures(XTest); 
