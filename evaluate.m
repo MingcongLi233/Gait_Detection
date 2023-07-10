b7338d7543aab00e3076c7aeb37cfc676c3aa9c1
Yueqiu Wang <ge23jiv@tum.de>
2023-07-10 20:55:08 +0200

    add comment

--- evaluate.m
+++ evaluate.m
@@ -1,4 +1,18 @@
 function accuracy = evaluate(YPred,YTest)
+% This function is used to evaluate the trained model
+%
+% INPUTS: 
+% - YPred: predicted labels obtaining after classification
+% - YTest: true labels of the test data
+%
+% OUTPUT:
+% - accuracy: how good is the predction ability from the trained model
+%   accuracy = (number of correct predictions)/(number of predictions)
+% - confusionchart: a confusion matrix chart from true labels YTest and 
+%   predicted labels predictedLabels and returns a ConfusionMatrixChart object. 
+%   The rows of the confusion matrix correspond to the true class and the columns 
+%   correspond to the predicted class. Diagonal and off-diagonal cells correspond 
+%   to correctly and incorrectly classified observations, respectively.
 
 
 % compute the accuracy of the model
